aafb43af824f01db5100f1971346f8890fec0d10
Sasara Kavishan <sasarakavishan2001528@gmail.com>
2025-08-17 19:57:40 +0530

    Add Item class and update Customer class structure

--- src/main/java/com/pahanaedu/bookshop/model/Customer.java
+++ src/main/java/com/pahanaedu/bookshop/model/Customer.java
@@ -5,27 +5,43 @@ public class Customer {
     private String name;
     private String address;
     private String telephone;
-    private int unitsConsumed;
 
-    public Customer() {}
-
-    public Customer(String accountNumber, String name, String address, String telephone, int unitsConsumed) {
+    public Customer(String accountNumber, String name, String address, String telephone) {
         this.accountNumber = accountNumber;
         this.name = name;
         this.address = address;
         this.telephone = telephone;
-        this.unitsConsumed = unitsConsumed;
     }
 
-    // Getters and Setters
-    public String getAccountNumber() { return accountNumber; }
-    public void setAccountNumber(String accountNumber) { this.accountNumber = accountNumber; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
-    public String getTelephone() { return telephone; }
-    public void setTelephone(String telephone) { this.telephone = telephone; }
-    public int getUnitsConsumed() { return unitsConsumed; }
-    public void setUnitsConsumed(int unitsConsumed) { this.unitsConsumed = unitsConsumed; }
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }
